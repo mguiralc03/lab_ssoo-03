1e0733a6ab96c829f853a60ffe5b993cc190020b
marta <100429126@alumnos.uc3m.es>
2021-05-04 20:37:50 +0200

    Creating the queue v 1.1

--- queue.c
+++ queue.c
@@ -1,6 +1,3 @@
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,15 +7,27 @@
 
 
 //To create a queue
-queue* queue_init(int size){
-
-    queue * q = (queue *)malloc(sizeof(queue));
+queue* queue_init(int capacity){
+    //Initializing all parameters in the queue
+    queue *q = (queue *)malloc(sizeof(queue));
+    q -> array = malloc(sizeof(struct element)*capacity);
+    q -> capacity = capacity;
+    q -> size = 0;
+    q -> front = NULL;
+    q -> rear = NULL;
     return q;
 }
 
 
 // To Enqueue an element
-int queue_put(queue *q, struct element* x) {
+int queue_put(queue *q, struct element *x) {
+    if not (queue_full(q)){
+        if (queue_empty(q))
+            q -> front = *x;
+        q -> array[q -> size] = *x;
+        q -> size++;
+        q -> rear = *x;
+    }
     return 0;
 }
 
@@ -26,23 +35,30 @@ int queue_put(queue *q, struct element* x) {
 // To Dequeue an element.
 struct element* queue_get(queue *q) {
     struct element* element;
-
+    int i;
+    for (i = 1; i < (q -> size); i++){
+        q -> array[i-1] = q -> array[i];
+    }
+    q -> size--;
+    q -> first = q -> array[0];
+    q -> rear = q -> array[q -> size - 1];
+    q -> array[q -> size] = NULL
     return element;
 }
 
 
 //To check queue state
 int queue_empty(queue *q){
-
-    return 0;
+    return (size == 0);
 }
 
 int queue_full(queue *q){
-
-    return 0;
+    return (size == capacity);
 }
 
 //To destroy the queue and free the resources
 int queue_destroy(queue *q){
+    free(q -> array);
+    free(q);
     return 0;
 }
