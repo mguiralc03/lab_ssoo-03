139e4dae06fc848b2986e6d39bb6fc496565e0e4
marta <100429126@alumnos.uc3m.es>
2021-05-06 12:43:21 +0200

    creating threads for consumer and producer version 1.6

--- costCalculator.c
+++ costCalculator.c
@@ -22,26 +22,42 @@
  * @return
  */
 
-int producer(int id, int machine_type, int time_of_use){
+void* producer(void* array){
     //Write in the queue
 }
 
-int consumer(){
+void* consumer(){
     //read from the queue
 }
 
-int main (int argc, const char * argv[] ) {
+int main (int argc, const char * argv[]) {
     int total = 0;
     int i, num_lines;
     FILE *fd;
-    close(STDIN_FILENO);
+    pthread_t *producerth[argv[2]];
+    pthread_t *consumerth;
+    pthread_mutex_t m;
+    pthread_cond_t *conditions[argv[2]];
+    // opening the file where instructions are written
+    //if error returns an error message
     if ((fd = fopen(argv[1], O_RDONLY)) < 0){
         perror("Error opening the file");
     }
+    // taking the number of instructions to be read
     fscanf(fd, "%d", &num_lines);
+    //array of three positions, where instructions will be stored.
+    //1º position: id of instruction, 2º position: cost of instruction, 3º position: time of use
     int array[num_lines][3];
+    //loop iterating thorugh the instructions
     for (i = 0; i < num_lines; i++){
         fscanf(fd, "%d %d %d", &array[i][0], &array[i][1], &array[i][2]);
+    }
+    pthread_create(&consumerth, NULL, consumer, NULL);
+    for (j=0; j < num_lines; j = j + argv[2]){
+        for (i=0; i< argv[2]; i++){
+            if (j+i < num_lines)
+                pthread_create(&producerth[i], NULL, producer, array[j + i])
+        }
     }
     printf("Total: %i €.\n", total);
 
